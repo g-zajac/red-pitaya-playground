aa4718dfe83c7de7ff7f205d6e9a8517b70e7fb4
Grzegorz on Mac <grzegorz.zajac@me.com>
2020-02-27 09:42:37 +0100

    add led indicator and infinite loop

--- osc_send_test.py
+++ osc_send_test.py
@@ -3,6 +3,12 @@ from osc4py3.as_eventloop import *
 from osc4py3 import oscbuildparse
 
 import time
+import redpitaya_scpi as scpi
+
+rp_s = scpi.scpi('0')
+# clear all leds
+for i in range(8):
+    rp_s.tx_txt('DIG:PIN LED' + str(i) + ',' + str(0))
 
 # Start the system.
 osc_startup()
@@ -19,21 +25,33 @@ msg = oscbuildparse.OSCMessage("/test/me", None, ["text", 672, 8.871])
 osc_send(msg, "redpitaya")
 
 # Buils a complete bundle, and postpone its executions by 10 sec.
-exectime = time.time() + 10   # execute in 10 seconds
-msg1 = oscbuildparse.OSCMessage("/sound/levels", None, [1, 5, 3])
-msg2 = oscbuildparse.OSCMessage("/sound/bits", None, [32])
-msg3 = oscbuildparse.OSCMessage("/sound/freq", None, [42000])
-bun = oscbuildparse.OSCBundle(oscbuildparse.unixtime2timetag(exectime),
-                    [msg1, msg2, msg3])
-osc_send(bun, "redpitaya")
+# exectime = time.time() + 10   # execute in 10 seconds
+# msg1 = oscbuildparse.OSCMessage("/sound/levels", None, [1, 5, 3])
+# msg2 = oscbuildparse.OSCMessage("/sound/bits", None, [32])
+# msg3 = oscbuildparse.OSCMessage("/sound/freq", None, [42000])
+# bun = oscbuildparse.OSCBundle(oscbuildparse.unixtime2timetag(exectime),
+#                     [msg1, msg2, msg3])
+# osc_send(bun, "redpitaya")
 
 # Periodically call osc4py3 processing method in your event loop.
-finished = False
-while not finished:
-    # You can send OSC messages from your event loop too…
-    # …
-    osc_process()
-    # …
+# finished = False
+# while not finished:
+#     # You can send OSC messages from your event loop too…
+#     # …
+#     rp_s.tx_txt('DIG:PIN LED1,1')
+#     osc_process()
+#     # …
+#
+# # Properly close the system.
+# osc_terminate()
 
-# Properly close the system.
-osc_terminate()
+
+while(True):
+    rp_s.tx_txt('DIG:PIN LED1,1')
+    print("looping and sending osc, test")
+    msg = oscbuildparse.OSCMessage("/test/me", ",sif", ["text", 672, 8.871])
+    osc_send(msg, "redpitaya")
+    osc_process()
+    # time.sleep(1)
+    rp_s.tx_txt('DIG:PIN LED1,0')
+    time.sleep(0.5)
